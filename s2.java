865310b8e3fe03836cf3ddd08d294af8f4653f94
logambika <logambika611@gmail.com>
2016-08-09 15:52:10 +0530

    Create s2.java

---
+++ s2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		char a='c';
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+System.out.print("vowelse");
+else
+System.out.print("constant");
+	}
+}
